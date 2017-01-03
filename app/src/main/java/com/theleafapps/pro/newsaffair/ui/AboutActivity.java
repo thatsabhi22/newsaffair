c8a1e5d396b9443044c4f88cd01c0e4af9f54b47
The Aviator <abhi@xyz>
2017-01-03 06:30:51 +0530

    AboutActivity, CreditsActivity updated with noactionbar theme Added a style for no action bar

--- app/src/main/java/com/theleafapps/pro/newsaffair/ui/AboutActivity.java
+++ app/src/main/java/com/theleafapps/pro/newsaffair/ui/AboutActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 
 import com.theleafapps.pro.newsaffair.R;
 
@@ -15,30 +14,25 @@ public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_about);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            View aboutPage = new AboutPage(this)
+                    .isRTL(false)
+                    .setImage(R.mipmap.ic_launcher)
+                    .setDescription("News Affair\nNews Affair is the one stop destination to popular news channels on the globe.\n" +
+                            "Catch with the latest stories and happenings around the various news categories that includes world, business, blog, photography, technology, sport, film, comics, science, and local, national and international news.")
+                    .addItem(new Element().setTitle("Version 1.0"))
+                    .addGroup("Connect with us")
+                    .addEmail("getintouch@theleafapps.com")
+                    .addWebsite("http://theleafapps.com/")
+                    .addFacebook("theleafapps")
+                    .addTwitter("theleafapps")
+                    .addPlayStore("com.theleafapps.pro")
+                    .addInstagram("theleafapps")
+                    .create();
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-        View aboutPage = new AboutPage(this)
-                .isRTL(false)
-                .setImage(R.mipmap.ic_launcher)
-                .setDescription("News Affair\nNews Affair is the one stop destination to popular news channels on the globe.\n" +
-                        "\n" +
-                        "Catch with the latest stories and happenings around the various news categories that includes world, business, blog, photography, technology, sport, film, comics, science, and local, national and international news.\n")
-                .addItem(new Element().setTitle("Version 1.0"))
-                .addGroup("Connect with us")
-                .addEmail("getintouch@theleafapps.com")
-                .addWebsite("http://theleafapps.com/")
-                .addFacebook("theleafapps")
-                .addTwitter("theleafapps")
-                .addPlayStore("com.theleafapps.pro")
-                .addInstagram("theleafapps")
-                .create();
-
-        setContentView(aboutPage);
+            setContentView(aboutPage);
     }
 
 
--- app/src/main/java/com/theleafapps/pro/newsaffair/ui/CreditsActivity.java
+++ app/src/main/java/com/theleafapps/pro/newsaffair/ui/CreditsActivity.java
@@ -3,7 +3,6 @@ package com.theleafapps.pro.newsaffair.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.Window;
 
 import com.theleafapps.pro.newsaffair.R;
 
@@ -13,9 +12,6 @@ public class CreditsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
     }
 
     @Override
